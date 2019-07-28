bc1fb553da3c775dd5fb819786b7953605ecaa1f
Urtzi Arana <44302701+UrtziFM@users.noreply.github.com>
2019-07-28 22:35:32 +0200

    Add files via upload

---
+++ practicajvs.js
@@ -0,0 +1,27 @@
+/* crear diferentes variables dentro de un script para la practica NVL-JS-P6a */
+
+var mi_nombre = "Urtzi";
+var mis_apellidos = ["Arana", "Santamaria"];
+var mi_edad = 38;
+var dias_semana = ["L", "M", "X", "J", "V", "S", "D"];
+var gravedad$ = 1;
+var impares_numeros = [1, 3, 5];
+var cadena_texto = "hello \"world\"";
+
+/*interactuando con las variables */
+
+mi_edad += 5;
+mi_edad++;
+mi_edad *= 2;
+alert(typeof (dias_semana));
+mi_edad === "5";
+alert(mi_nombre + mis_apellidos);
+alert(mi_nombre.length);
+
+/* practica con cadena string */
+
+var mi_cadena = "Amigo";
+
+alert(mi_cadena.toLocaleLowerCase());
+alert(mi_cadena.replace("A", "a"));
+alert(mi_cadena.toLowerCase());
